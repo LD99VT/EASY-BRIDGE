8d52aac305ae8757c3b2628118b0c454303d2f2a
Vasyl Tonkonoh <ld99@Vasyls-MacBook-Pro.local>
2026-03-04 18:14:48 +0100

    Use source icns only on macOS

--- cmake/GenerateIconsFromPng.cmake
+++ cmake/GenerateIconsFromPng.cmake
@@ -25,24 +25,6 @@ if(NOT _img_convert)
   return()
 endif()
 
-set(_icns_png_abs "${_png_abs}")
-if(APPLE)
-  set(_bridge_icon_prep "${CMAKE_CURRENT_SOURCE_DIR}/cmake/generate_bridge_macos_icon.swift")
-  find_program(_xcrun NAMES xcrun)
-  if(EXISTS "${_bridge_icon_prep}" AND _xcrun)
-    get_filename_component(_png_name_we "${PNG_ICON_SOURCE}" NAME_WE)
-    set(_icns_png_abs "${CMAKE_CURRENT_BINARY_DIR}/generated/${_png_name_we} macOS.png")
-    add_custom_command(OUTPUT "${_icns_png_abs}"
-      COMMAND "${CMAKE_COMMAND}" -E make_directory "${CMAKE_CURRENT_BINARY_DIR}/generated"
-      COMMAND "${_xcrun}" swift "${_bridge_icon_prep}" "${_png_abs}" "${_icns_png_abs}"
-      MAIN_DEPENDENCY "${_png_abs}"
-      DEPENDS "${_bridge_icon_prep}"
-      COMMENT "Preparing macOS app icon source from ${PNG_ICON_SOURCE}"
-      VERBATIM
-    )
-  endif()
-endif()
-
 string(REPLACE " " "_" _ico_stem "${ICON_OUTPUT_BASE}")
 set(_gen_dir "${CMAKE_CURRENT_BINARY_DIR}/generated")
 set(_ico_out "${_gen_dir}/${_ico_stem}.ico")
@@ -62,43 +44,11 @@ if(WIN32)
 endif()
 
 set(GENERATED_ICNS_PATH "")
-if(APPLE)
-  set(_icns_stem "${ICON_BUNDLE_NAME}")
-  if(NOT _icns_stem)
-    set(_icns_stem "${ICON_OUTPUT_BASE}")
-  endif()
-  set(_icns_out "${_gen_dir}/${_icns_stem}.icns")
-  set(GENERATED_ICNS_PATH "${_icns_out}")
-  set(_iconset "${_gen_dir}/${_icns_stem}.iconset")
-  set(_deplist "")
-  foreach(_s 16 32 128 256 512)
-    math(EXPR _s2 "${_s} * 2")
-    set(_p1 "${_iconset}/icon_${_s}x${_s}.png")
-    set(_p2 "${_iconset}/icon_${_s}x${_s}@2x.png")
-    list(APPEND _deplist "${_p1}" "${_p2}")
-    add_custom_command(OUTPUT "${_p1}"
-      COMMAND "${CMAKE_COMMAND}" -E make_directory "${_iconset}"
-      COMMAND "${_img_convert}" "${_icns_png_abs}" -resize "${_s}x${_s}" "${_p1}"
-      MAIN_DEPENDENCY "${_icns_png_abs}"
-      COMMENT "ICNS iconset ${_p1}"
-      VERBATIM
-    )
-    add_custom_command(OUTPUT "${_p2}"
-      COMMAND "${_img_convert}" "${_icns_png_abs}" -resize "${_s2}x${_s2}" "${_p2}"
-      MAIN_DEPENDENCY "${_icns_png_abs}"
-      COMMENT "ICNS iconset ${_p2}"
-      VERBATIM
-    )
-  endforeach()
-  add_custom_command(OUTPUT "${_icns_out}"
-    COMMAND iconutil --convert icns --output "${_icns_out}" "${_iconset}"
-    DEPENDS ${_deplist}
-    MAIN_DEPENDENCY "${_icns_png_abs}"
-    COMMENT "Generating .icns from ${PNG_ICON_SOURCE}"
-    VERBATIM
-  )
+set(USE_GENERATED_ICONS FALSE)
+if(WIN32)
+  set(USE_GENERATED_ICONS TRUE)
 endif()
-
-set(USE_GENERATED_ICONS TRUE)
 set(GENERATED_ICO_FILENAME "${_ico_stem}.ico")
-message(STATUS "Icons will be generated from ${PNG_ICON_SOURCE} (ico + icns on macOS)")
+if(WIN32)
+  message(STATUS "Windows icon will be generated from ${PNG_ICON_SOURCE} (.ico)")
+endif()
